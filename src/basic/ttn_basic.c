005d537966a6875350a7a5dd2bbb7a10893edaec
tickelton <tickelton@gmail.com>
2021-01-12 00:29:57 +0100

    Adds buffer manager POC

--- src/basic/ttn_basic.c
+++ src/basic/ttn_basic.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define PROG_BUFFER_LEN 100
+char *prog_buffer[PROG_BUFFER_LEN] = {0};
+
 
 void add(int index, char *program)
 {
-    printf("add: '%d' -> '%s'\n", index, program);
+    if(prog_buffer[index]) {
+        free(prog_buffer[index]);
+        prog_buffer[index] = 0;
+    }
+    prog_buffer[index] = strdup(program);
     free(program);
 }
 
 void list()
 {
-
+    for(int i=0; i< PROG_BUFFER_LEN; ++i) {
+        if(prog_buffer[i]) {
+            printf("%d\t%s\n", i, prog_buffer[i]);
+        }
+    }
 }
 
 void execute()
 {
-
+    for(int i=0; i< PROG_BUFFER_LEN; ++i) {
+        if(prog_buffer[i]) {
+            if(strlen(prog_buffer[i]) > 5 && !strncmp(prog_buffer[i], "PRINT", 5)) {
+                printf("\t%s\n", prog_buffer[i]+6);
+            }
+        }
+    }
 }
 
 
